20c68de80bc605810903f18c5ea8ed2bdcf86e47
VIGNESH G <117510243+vignesh220904@users.noreply.github.com>
2026-01-19 14:18:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/redrhino-digital-india-launch/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
